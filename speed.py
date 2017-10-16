0d9770c714c2caae225574ab41012c8d240c1505
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-10-16 04:26:08 -0200

    Add MetricWriterCSV.

--- speed.py
+++ speed.py
@@ -2,9 +2,13 @@
 
 
 import pyspeedtest as pst
+import os
 import time
 from collections import namedtuple
 from datetime import datetime
+
+
+METRICS = ["time", "ping", "upload", "download", "status"]
 
 
 class Metric:
@@ -14,7 +18,7 @@ class Metric:
         self.upload = upload
         self.download = download
         self.status_ok = status_ok
-        self.time = datetime.now()
+        self.time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         self._convert_time_units(unit_time)
         self._convert_bandwitdh_units(unit_bandwidth)
@@ -32,13 +36,45 @@ class Metric:
     def __str__(self):
         if self.status_ok:
             return "{} - ping: {:.2f} ms, upload: {:.2f} Mbps, download: {:.2f} Mbps"\
-                .format(self.time.strftime("%Y-%m-%d %H:%M:%S"), self.ping, self.upload, self.download)
+                .format(self.time, self.ping, self.upload, self.download)
         else:
-            return "{} - no connection".format(self.time.strftime("%Y-%m-%d %H:%M:%S"))
+            return "{} - no connection".format(self.time)
+
+    def __iter__(self):
+        yield self.time
+        yield self.ping
+        yield self.upload
+        yield self.download
+        yield "online" if self.status_ok else "offline"
+
+
+class MetricWriterCSV:
+    def __init__(self, file):
+        self._file = file
+
+        if not os.path.isfile(self._file) or os.stat(self._file).st_size == 0:
+            self._add_header()
+
+    def write(self, metric):
+        with open(self._file, 'a') as f:
+            line = self._format(metric)
+            f.write(line + '\n')
+
+    def _add_header(self):
+        with open(self._file, 'w') as f:
+            header = ",".join(METRICS)
+            f.write(header + '\n')
+
+    def _format(self, metric):
+        metric_str = (str(m) for m in metric)
+        return ",".join(metric_str)
 
 
 def main():
     st = pst.SpeedTest()
+
+    csv_file = os.path.join(os.path.dirname(__file__), "metrics.csv")
+    csv_writer = MetricWriterCSV(csv_file)
 
     while True:
         try:
@@ -53,6 +89,7 @@ def main():
 
         metric = Metric(ping, upload, download, is_online)
 
+        csv_writer.write(metric)
         print(metric)
 
         time.sleep(20)
